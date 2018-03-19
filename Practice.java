74a0b1d5de0f5352f7a0e0944b503dc174e21a56
Patel, Krishna <krishna.patel@metlife.com>
2018-03-19 16:38:06 -0400

    occurrences

--- Practice.java
+++ Practice.java
@@ -10,12 +10,17 @@ public class Practice{
         System.out.printf("Before sort: %s", Arrays.toString(arr));
         System.out.println();
 
-       
+        for (int i = 0; i < arr.length; i++){
+            int occurs = countNumofOccurance(arr, arr[i]);
+            System.out.printf("\n%d found %d time(s) in array", arr[i], occurs);
+        }
+        
         Arrays.sort(arr);
 
-        System.out.printf("After sort: %s", Arrays.toString(arr));
+        System.out.printf("\n\nAfter sort: %s", Arrays.toString(arr));
 
         boolean dup = false; 
+
        for (int i = 0; i < arr.length; i++){
            for(int j = i + 1; j < arr.length; j++){
                 if(dup==false && (arr[i] == arr[j])){
@@ -24,5 +29,18 @@ public class Practice{
                 }
            }
        } 
+    
     }
+
+    public static int countNumofOccurance(int arr[], int value) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == value){
+              //  System.out.printf("%d found at arr[%d] \n", value, i);   
+                count++;
+             }
+        }  
+        return count;
+    }// end of countNumofOccurance
+
 } // end of class
