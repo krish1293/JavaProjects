3ae8ee3e8eb871c1af44251c48e5d3414a8f8581
Patel, Krishna <krishna.patel@metlife.com>
2018-04-02 15:34:34 -0400

    New code added

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -1,4 +1,5 @@
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+import java.util.Arrays;
+import java.util.Arrays.*;
 
 public class ArraysPractice{
 
@@ -8,10 +9,31 @@ public class ArraysPractice{
         int numNotFound = missingNum(arr, n);
         System.out.printf("%d is not found in array\n", numNotFound);
 
+        int arr2[] = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 15;
+
+        // use this if subArraySum was no defined as a static method
+        //  ArraysPractice arrSum = new ArraysPractice();
+        // arrSum.subArraySum(arr2, n, sum);
+        
+        int arrSum = subArraySum(arr2, n, sum);
+
+		int arr3[] = new int[] {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+		
+		Arrays.sort(arr3);
+		
+        System.out.printf("\n\nAfter sort: %s\n\n", Arrays.toString(arr3));
+        
+        int arr4[] = new int[]{16, 17, 4, 3, 5, 2};
+        leaders(arr4, arr4.length);
+
+        int arr5[] = new int[]{1,2,3,4,0};
+        leaders(arr5, arr5.length);
+
     } // end of main
 
     /*** 
-        missingNum takes to arguments an array and a number (n) (length of array + 1)
+        missingNum takes 2 arguments an array and a number (n) (length of array + 1)
         Array should have numbers 1 - n
         numTotal = sum of all numbers in the array
             to get the missing number subtract all the numbers in array from the 
@@ -25,5 +47,48 @@ public class ArraysPractice{
         }
         return numTotal;
     } //end of missingNum
+
+    /*** 
+        subArraySum takes 3 arguments an array, a number (n) (length of array + 1) and sum
+        Array should have numbers 1 - n
+        Finds the subarray with given sum
+   ****/
+    public static int subArraySum(int arr[], int n, int sum){
+        int currSum = 0;
+        for(int i = 0; i < n; i++){
+            currSum = arr[i];
+            for (int j = i + 1; j <= n; j++){
+                if (currSum == sum){
+                    int p = j - 1;
+                    System.out.printf("%d sum was found between %d and %d\n", currSum, i, p);
+                    return 1;
+                }
+                if(currSum > sum || j == n)   
+                    break;
+                currSum = currSum + arr[j];
+            }
+        }
+
+        System.out.println("No Subarray found");
+        return 0;
+
+    } // end of subArraySum
+
+    public static void leaders(int arr[], int n){
+        int maxInt = 0;
+        for (int i = 0; i < n; i++){
+            maxInt = arr[i];
+            for (int j = i + 1; j < n; j++){
+                if (maxInt < arr[j]){
+                    maxInt = arr[j];
+                    System.out.printf("%d ", maxInt);
+                }
+            }
+            maxInt = arr[n-1];
+
+            //System.out.println(maxInt);
+        }
+        System.out.printf("%d \n", maxInt);
+    } // end of leaders
 
 } // end of ArraysPractice
