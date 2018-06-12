a8253464a040b452b4b982444e2b3efa53db3e33
Patel,Krishna <krishna.patel@metlife.com>
2018-06-12 17:00:28 -0400

    new set of questions

---
+++ New Problem sets/moreCoding.java
@@ -0,0 +1,32 @@
+public  class moreCoding() {
+    
+    public static void main(String[] args) {
+
+    } // main
+
+    // 1. Reverse linked list
+
+    // 2. Given an array and int m, find avarage of m groups in o(n)
+
+    // 3. Given an array and target, find all pairs whose sum is equal to target.
+
+    // 4. Brackets Matching
+
+    // 5. Pair of range, merge them
+
+    // 6. Add two linked lists
+
+    // 7. First unique character in a string
+
+    // 8. Write a functions that finds and returns the node with the second highest value in a Binary Search Tree. 
+    // Assume BST is valid, but not necessarily complete or balanced.
+
+    // 9. Largest sum of path in 2D array.
+
+    // 10. Reverse the order of words in a sentence.
+
+    // 11. Validate BST
+
+    // 12. 
+
+} // moreCoding
